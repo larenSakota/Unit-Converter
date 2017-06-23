5bf5cc9881b4e5039daae41d9301dfc70994796e
Laren Sakota <jedi.temple@yahoo.com>
2017-06-22 21:21:18 -0700

    completed initial converter.  will continue to build out additional features

--- Unit Converter/ViewController.swift
+++ Unit Converter/ViewController.swift
@@ -8,16 +8,54 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let list = Unit.allCases()
+    
+    @IBOutlet weak var picker: UIPickerView!
+    @IBOutlet weak var output: UITextField!
+    @IBOutlet weak var input: UITextField!
+    
+    @IBAction func convertUnits(_ sender: Any) {
+        let fromUnitIdx = picker.selectedRow(inComponent: 0)
+        let toUnitIdx = picker.selectedRow(inComponent: 1)
+        
+        let fromUnit = Unit.fromString(list[fromUnitIdx])!
+        let toUnit = Unit.fromString(list[toUnitIdx])!
+        
+        if let inputText = input.text {
+            if !inputText.isEmpty {
+                let inputNum = Double(inputText)!
+                let outputNum = fromUnit.convertTo(unit: toUnit, value: inputNum)
+                output.text = String(describing: outputNum)
+                
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        picker.delegate = self
+        picker.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 2
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return list.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return list[row]
     }
 
 
